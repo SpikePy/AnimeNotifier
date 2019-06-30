32e45a68c5147b095dbd83b61cc54ebab8acfb64
René Hoffmann <rho@mail.de>
2019-06-30 13:23:35 +0200

    improve html parsing

--- OnePunch.sh
+++ OnePunch.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env sh
 clear
-url=https://mangadex.org/title/7139/one-punch-man
+URL=https://mangadex.org/title/7139/one-punch-man
+SUBSCRIBERS="hoffelmann@gmail.com" # Separate multiple users with coma inside the quotation marks
 
-latest_read=$(tail -1 $0 | cut -c2-)
-#echo $latest_read
+# Get latest_notified Chapters
+latest_notified=$(tail -1 $0 | cut -c2-)
+#echo $latest_notified
 
-html_overview=$(curl -s $url)
-# Get latest_read Chapters
-latest_online=$(echo $html_overview | grep "/chapter/[0-9]*'" | grep -Po '(?<=Ch. )[0-9.]+' | sort -nu | tail -1)
+# Store page in variable
+html=$(curl -s $URL)
+
+latest_online=$(echo "$html" | grep -EA 8 "href='/chapter/[0-9]+" | grep -B 8 "title='English'" | head -1 | grep -Po "(?<=href=').*?(?=')")  # Parse first english link to episode on page
+latest_online_title=$(echo "$html" | grep -EA 8 "href='/chapter/[0-9]+" | grep -B 8 "title='English'" | head -1 | grep -Po "(?<=>).*?(?=</a)") # Parse first english title of episode on page
 #echo $latest_online
 
-if [ "$latest_online" != "$latest_read" ]; then
-	echo "One Punch Man Chapter $latest_online is now available!"
-	href=$(echo "$html_overview" | grep -EA 8 "href='/chapter/[0-9]+.*$latest_online" | grep -B 8 "title='English'" | head -1 | grep -Po "(?<=href=').*?(?=')")
+if [ "$latest_online" != "$latest_notified" ]; then
+	echo "New chapter online: One Punch Man $latest_online_title"
+	href=$(echo "$html" | grep -EA 8 "href='/chapter/[0-9]+.*$latest_online" | grep -B 8 "title='English'" | head -1 | grep -Po "(?<=href=').*?(?=')")
 
-	echo "#$latest_online" >> $0 # Append current episode to
-	#echo "$url$href"   # Print URL of current episode to terminal
-	#firefox $url$href  # open new episode in firefox
-	echo "Link to current episode: $url$href" | mail -s "One Punch Man $latest_online was just released" hoffelmann@gmail.com
+	echo "#$latest_online" >> $0 # Append current episode to file
+	#echo "$URL$href"   # Print URL of current episode to terminal
+	#firefox $URL$href  # open new episode in firefox
+	echo "Read it on: $URL$href" | mail -s "One Punch Man $latest_online_title was just released" $SUBSCRIBERS
 else
-	echo "Nothing new :("
+	echo "No new chapter online"
 fi
 
-# Known chapters
-#109
-#110
+# Already Notified
+#/chapter/642878
