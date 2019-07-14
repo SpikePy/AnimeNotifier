be4fcd31e5054fee7b2056606048da66ae60d38b
Spike Py <hoffelmann@gmail.com>
2019-07-14 08:00:34 +0200

    improve documentation

--- OnePunch.sh
+++ OnePunch.sh
@@ -1,29 +1,39 @@
 #!/usr/bin/env sh
 clear
 
-NOTIFY_TO="hoffelmann@gmail.com" # Separate multiple users with coma inside the quotation marks
+MANGA_RSS='https://mangadex.org/rss/cknpMfm3XeCgAdyaru2sT4EzqhxPFNVv/manga_id/7139'
+SUBSCRIBERS="hoffelmann@gmail.com" # Separate multiple users with coma inside the quotation marks
 
-MANGA='OnePunchMan' # Name of the manga in the describtion tag of the rss feed
-RSS_ALL_URL='https://mangadex.org/rss/follows/cknpMfm3XeCgAdyaru2sT4EzqhxPFNVv'
-RSS_MANGA_DATA=$(curl -s "$RSS_ALL_URL" | grep -B 3 "$MANGA")
+# Download RSS feed and save it to a variable
+RSS_MANGA_DATA=$(curl -s "$MANGA_RSS")
 
-# Get latest_notified Chapters
-LATEST_NOTIFIED=$(tail -1 $0 | cut -c2-)
+# Parse latest notified chapters from this very file
+LATEST_LOCAL_URL=$(tail -1 $0 | cut -c2-)
 #echo $latest_notified
 
-LATEST_ONLINE_TITLE=$(echo "$RSS_MANGA_DATA" | sed -n 1p | grep -Po "(?<=>).*?(?=<)")
-LATEST_ONLINE_URL=$(echo "$RSS_MANGA_DATA" | sed -n 2p | grep -Po "(?<=>).*?(?=<)")
+# Parse most recent online episode from RSS feed
+LATEST_ONLINE_URL=$(echo "$RSS_MANGA_DATA" | sed -n 13p | grep -Po "(?<=>).*?(?=<)")
 
-if [ "$LATEST_ONLINE_URL" != "$LATEST_NOTIFIED" ]; then
-	echo "New chapter online: One Punch Man $LATEST_ONLINE_TITLE"
+if [ "$LATEST_ONLINE_URL" != "$LATEST_LOCAL_URL" ]; then
 
-	echo "#$LATEST_ONLINE_URL" >> $0 # Append current episode to file
+    # Parse RSS feed data of most recent episode
+    LATEST_ONLINE_TITLE=$(echo "$RSS_MANGA_DATA" | sed -n 12p | grep -Po "(?<=>).*?(?=<)")
+    echo "New chapter online: $LATEST_ONLINE_TITLE"
+
+    # Append new online episode url to this file
+    echo "#$LATEST_ONLINE_URL" >> $0
+
 	#echo "$URL$href"   # Print URL of current episode to terminal
 	#firefox $URL$href  # open new episode in firefox
-	echo "Read it on: $LATEST_ONLINE_URL" | mail -s "$LATEST_ONLINE_TITLE was just released" $NOTIFY_TO
+
+    # Send e-mail notification to SUBSCRIBERS
+    echo "Read it on: $LATEST_ONLINE_URL" | mail -s "$LATEST_ONLINE_TITLE was just released" $SUBSCRIBERS
 else
 	echo "No new chapter online"
 fi
 
 # Already Notified
+#===================================
 #https://mangadex.org/chapter/653856
+#https://mangadex.org/chapter/660185
+#https://mangadex.org/chapter/660875
